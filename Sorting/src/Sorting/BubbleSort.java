c149fb24cc83d89e75c285341a101e3dec8ccd3b
vanyacohen <vanya_cohen@brown.edu>
2015-04-09 18:56:45 -0400

    Added bubblesort

--- Sorting/src/Sorting/BubbleSort.java
+++ Sorting/src/Sorting/BubbleSort.java
@@ -8,7 +8,17 @@ public class BubbleSort {
 	 */
 	public static int[] bubbleSort(int[] unsortedArray) {
 		//TODO: S1 implements bubble sort
-		
-		return null;
+
+		for (int i = 0; i < unsortedArray.length; i++) {
+			for (int j = 0; j < unsortedArray.length - 1; j++) {
+				if (unsortedArray[j] > unsortedArray[j + 1]) {
+					int temp = unsortedArray[j];
+					unsortedArray[j] = unsortedArray[j + 1];
+					unsortedArray[j + 1] = temp;
+				}
+			}
+		}
+
+		return unsortedArray;
 	}
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -22,8 +22,13 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
+		unsortedArray = BubbleSort.bubbleSort(unsortedArray);
+
+		for (int i: unsortedArray) {
+				System.out.print(i + " ");
+		}
 		
-		
+		System.out.println();
 	}
 
 }
